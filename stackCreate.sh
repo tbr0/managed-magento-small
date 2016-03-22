d30dfbce1eb5b836a2bdfac2b0cce0d00e58e71d
tbr0 <taylor.broesche@rackspace.com>
2016-03-22 13:31:11 -0500

    Attempting to get managed lamp kick to run.

--- stackCreate.sh
+++ stackCreate.sh
@@ -1,3 +1,3 @@
 heat stack-create -u "https://raw.githubusercontent.com/tbr0/managed-magento-small/master/managed-magento-small.yml" \
 -P 'magento_url=example.com;server_count=1;magento_eula=1' \
-stack01-example.com
+stack02-example.com
