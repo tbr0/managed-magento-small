53e9eefa884fa46b0483834400cc5c767bacfefd
tbr0 <taylor.broesche@rackspace.com>
2016-03-24 13:26:17 -0500

    Now using user-data to perform software deployment.

--- stackCreate.sh
+++ stackCreate.sh
@@ -1,3 +1,4 @@
-heat stack-create -u "https://raw.githubusercontent.com/tbr0/managed-magento-small/master/managed-magento-small.yml" \
--P 'magento_url=example.com;server_count=1;magento_eula=1' \
+#heat stack-create -u "https://raw.githubusercontent.com/tbr0/managed-magento-small/master/managed-magento-small.yml" \
+heat stack-create -f "managed-magento-small.yml" \
+-P 'magento_url=example.com;server_count=0;magento_eula=1' \
 stack01-example.com
