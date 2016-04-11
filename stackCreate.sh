91fcce71c3d91f56ebacb4a56116ea929061a3cf
tbr0 <taylor.broesche@rackspace.com>
2016-04-11 13:39:40 -0500

    Testing secondary, removing str_replace for now.

--- stackCreate.sh
+++ stackCreate.sh
@@ -1,4 +1,4 @@
 #heat stack-create -u "https://raw.githubusercontent.com/tbr0/managed-magento-small/master/managed-magento-small.yml" \
 heat stack-create -f "managed-magento-small.yml" \
--P 'magento_url=example.com;server_count=0;magento_eula=1' \
+-P 'magento_url=example.com;magento_eula=1' \
 stack01-example.com
