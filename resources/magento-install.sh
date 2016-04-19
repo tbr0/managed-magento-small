a972121c4a58c5a1ad0800e1dabee8bb239f0fb7
tbr0 <taylor.broesche@rackspace.com>
2016-04-19 12:35:02 -0500

    Adding parameters for Magento credentials and info.

--- resources/magento-install.sh
+++ resources/magento-install.sh
@@ -4,6 +4,11 @@ DB_HOST=${2}
 DB_NAME=${3}
 DB_USER=${4}
 DB_PASS=${5}
+M_FNAME=${6}
+M_LNAME=${7}
+M_EMAIL=${8}
+M_USER=${9}
+M_PASS=${10}
 
 echo "Need to disable requiretty so we can run commands as Apache." && \
 sed -i /etc/sudoers -e "s/Defaults    requiretty/#Defaults     requiretty/g" && \
@@ -35,11 +40,11 @@ sudo -u apache /bin/php -f /var/www/vhosts/${DOMAIN}/install.php -- \
 --use_secure "no" \
 --secure_base_url "no" \
 --use_secure_admin "no" \
---admin_firstname "Joe" \
---admin_lastname "User" \
---admin_email "admin@example.com" \
---admin_username "admin" \
---admin_password "test123" \
+--admin_firstname "${M_FNAME}" \
+--admin_lastname "${M_LNAME}" \
+--admin_email "${M_EMAIL}" \
+--admin_username "${M_USER}" \
+--admin_password "${M_PASS}" \
 echo "Changing sudoers file back to normal." && \
 sed -i /etc/sudoers -e "s/#Defaults    requiretty/Defaults     requiretty/g" && \
 echo "Done."
